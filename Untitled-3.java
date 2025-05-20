1c7889dd69f7796cf4986a79a9124b3eb9a05d1c
eduardodefarias <145899398+eduardodefarias@users.noreply.github.com>
2025-05-20 16:48:05 -0300

    Add files via upload

---
+++ Untitled-3.java
@@ -0,0 +1,13 @@
+// dinamico
+
+const alunos = ["Bia", "Carlos", "Ana"]
+
+// push ()
+alunos.push("Marcos");
+alunos.push("Antônio", "Clara"));
+
+console.log(alunos)
+
+
+
+
---
+++ Untitled-4.js
@@ -0,0 +1,34 @@
+const tarefas = ["Estudar", "Treinar", "Ler"];  
+
+gerenciarTarefas(tarefas, "adicionarFim", "Dormir");  
+
+console.log(tarefas);
+
+// Resultado esperado: ["Estudar", "Treinar", "Ler", "Dormir"
+
+
+
+function gerenciarTarefas(tarefas, ação,nova tarefa)[
+    switch(ação)]
+    case "adicionarInicio"
+    if(NovaTarefa !==undefined)[
+    tarefas.push(NovaTarefa;)
+    ]
+    break;
+    case"RemoverInicio"
+    tarefas.shift();
+    break;
+    case"removerFim";
+    tarefas.pop();
+    break;
+    default:
+        console.log("Ação Invalida.");
+
+
+
+
+
+
+        const numeros = [10, 20, 30, 40, 50,];
+
+        for (let i = 0; 1 )
