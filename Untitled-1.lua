beb93481e3a4004c16e43feb5a2493f65a31c9c0
eduardodefarias <145899398+eduardodefarias@users.noreply.github.com>
2025-05-22 16:40:43 -0300

    Add files via upload

---
+++ Untitled-1.lua
@@ -0,0 +1,64 @@
+ 
+const produto2 = new produto("notebook" , 3500)
+const pruduto1 = new produto("celular" , 1500)
+const produto3 = new produto("Computador", 4500)
+// literal
+const aluno = 
+     nome: "Lucas",
+     nota: 7,5
+];
+
+console.log(aluno);
+
+// new
+const carro = new Object();
+
+
+
+function CriarUsuario( nome, Idade, email){
+    this.nome = nome
+    this.idade = Idade
+    this.email = email
+}
+
+const usuario = new criarUsuario("Jorge", 17, "jorginho@hotmail.com");
+console.log(usuario)
+
+
+const produto = {
+nome: "Celular",
+preco: 2000,
+
+categorias: ["Eletronico", "Tecnologia"],
+
+fabricante: {
+    nome: "Microsoft",
+    pais: "EUA"
+},
+
+aplicarDesconto: function(percentual){
+    return
+}
+
+
+
+function atualizarIdade(usuario, novaIdade){
+    usuario.idade = novaIdade
+}
+
+const usuario = new {nome: "Marcos", idade: 19, email: "marquinhos@hotmail.com"};
+atualizarIdade(usuario, 25);
+
+console.log(usuario.idade);
+
+
+
+
+
+
+
+
+
+
+
+
