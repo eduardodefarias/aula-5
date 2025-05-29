ed951f37bfdd98d58c52b94383f0d9e95dde84ae
eduardodefarias <145899398+eduardodefarias@users.noreply.github.com>
2025-05-29 16:51:13 -0300

    Add files via upload

---
+++ let contadorLike = 0;.js
@@ -0,0 +1,36 @@
+let contadorLike = 0;
+let contadorDeslike = 0;
+let contadorFavorito = 0;
+
+function async (params)  {
+    RegistrarLike
+contadorLike++;
+document.getElementByld("contadorLike").textContent = contadorLike;
+}
+ 
+function registrarDeslike(){
+    contadorDeslike++;
+    document.getElementByld("contadorDeslike").textContent = contadorDeslike;
+}
+
+function async () {
+    registrarFavorito
+}o()
+contadorFavorito++;
+document.getElementByld("contadorFavorito").textContent = contadorFavorito;
+
+function async (params) {
+    limparRegistro
+contadorlimparRegistro++;
+document.getElementByld("contadorlimparRegistro").textContent = contadorlimparRegistro
+}
+
+
+
+
+
+
+
+
+
+
